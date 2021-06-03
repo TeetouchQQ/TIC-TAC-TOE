1570eb388080839b35cda39f1379e157d6977e6f
Teetouch <teawkung@live.com>
2021-06-03 18:04:15 +0700

    add game grid

---
+++ script.js
@@ -0,0 +1,12 @@
+var game_display = document.getElementById('show-grid');
+var game_slot = [
+    '','','',//
+    '','','',//
+    '','','' //
+
+]
+function changeDisplay()
+function go(id){
+    console.log(id);
+
+}
