09bb8119563719d39fbd4d1057af1bd7a4f9eedc
Teetouch <teawkung@live.com>
2021-06-03 21:38:40 +0700

    swap turn pick

--- script.js
+++ script.js
@@ -5,8 +5,57 @@ var game_slot = [
     '','','' //
 
 ]
-function changeDisplay()
+player_1 = true;
+player_2 = false;
+
+function SwapTurn(){
+    if(player_1 == false){
+        player_1 = true;
+        player_2 = false;
+    }else{
+        player_1 = false;
+        player_2 = true;
+    }
+    console.log('swap')
+}
+
+function checkDisplay(slot){
+    if (game_slot[slot] == ''){
+        return true;
+    }else{
+        return false;
+    }
+    
+}
+
+function changeDisplay(mark,slot){
+    if (checkDisplay(slot)){
+        game_slot[slot] = mark
+        var grid = document.getElementById(slot);
+        grid.innerHTML = mark
+    }
+
+
+}
+function win_check(){
+
+
+}
+
 function go(id){
-    console.log(id);
+    console.log(game_slot);
+    if(checkDisplay(id) == true){
+        if(player_1 == true){
+            console.log('Player 1 Turn!')
+            changeDisplay('X',id)
+
+        }else{
+            changeDisplay('O',id)
+            console.log('Player 2 Turn!')
+        }
+        SwapTurn();
+    }
+    
+    
 
 }
